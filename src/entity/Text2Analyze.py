918cb400bc6055f02dd485e4caaf30ef4c119f0f
davel-go <joel.dav.gomez@gmail.com>
2024-12-01 21:52:46 -0600

    Improved show()

--- src/entity/Text2Analyze.py
+++ src/entity/Text2Analyze.py
@@ -8,6 +8,7 @@ class Text2Analyze:
         paragraphs = re.split(r'\n+', text)
         self.content = text
         self.paragraphs: [Paragraph] = [Paragraph(p.strip()) for p in paragraphs if p.strip()]
+        self.senteces_per_paragraph = [len(p.sentences) for p in self.paragraphs]
 
     def pause_positions(self):
         return [item for p in self.paragraphs for item in p.pause_positions]
@@ -23,13 +24,19 @@ class Text2Analyze:
     def char_count(self):
         return sum([sum(p.char_count) for p in self.paragraphs])
 
-    def avg_word_len(self):
-        return sum([p.avg_word_len for p in self.paragraphs]) / len(self.paragraphs)
-
     def word_len_list(self):
         ls = list()
         for p in self.paragraphs: ls += p.word_len_list
         return ls
+
+    def sentence_count(self):
+        return sum(self.senteces_per_paragraph)
+
+    def avg_word_len(self):
+        return round(sum([p.avg_word_len for p in self.paragraphs]) / len(self.paragraphs), 2)
+
+    def avg_senteces_per_paragraph(self):
+        return round(self.sentence_count() / len(self.paragraphs), 2)
 
     def __str__(self):
         txt = ""
@@ -40,15 +47,19 @@ class Text2Analyze:
     def show_stats(self):
         char_count = self.char_count()
         word_count = self.word_count_list()
+
         txt = f'''
-        Character count in this text: {char_count}
-        Word count in this text: {sum(word_count)}
-        Paragraph count: {len(self.paragraphs)}
-        =================
-        Word count list (words per sentence): {word_count}
-        Word lengths list (length of words): {self.word_len_list()}
-        Avg word length: {self.avg_word_len()}
-        =================
+        ============================================================
+        Paragraph count:    {len(self.paragraphs)}
+                Sentences in each paragraph:            {self.senteces_per_paragraph}
+                Average of sentences in each paragraph: {self.avg_senteces_per_paragraph()}
+        Sentence count:     {self.sentence_count()}
+        Character count:    {char_count}
+        Word count:         {sum(word_count)}
+                Word count list (words per sentence):   {word_count}
+                Word lengths list (length of words):    {self.word_len_list()}
+                Avg word length:                        {self.avg_word_len()}        
+        ============================================================
         Pause positions: {self.pause_positions()}
         '''
         print(txt)
