27141d880f0369f5e8d0da76fa666661686fd2a2
davel-go <joel.dav.gomez@gmail.com>
2024-12-07 23:13:44 -0600

    Added cat and cat_num models

--- src/util/data.py
+++ src/util/data.py
@@ -1,3 +1,43 @@
+import copy
+
+CAT = {
+    "VERB": [],
+    "NOUN": [],
+    "ADJ": [],
+    "ADV": [],
+    "PRON": [],
+    "DET": [],
+    "ADP": [],
+    "AUX": [],
+    "CONJ": [],
+    "NUM": [],
+    "PUNCT": [],
+    "OTHER": []
+}
+CAT_NUMS = {
+    "VERB": 0,
+    "NOUN": 0,
+    "ADJ": 0,
+    "ADV": 0,
+    "PRON": 0,
+    "DET": 0,
+    "ADP": 0,
+    "AUX": 0,
+    "CONJ": 0,
+    "NUM": 0,
+    "PUNCT": 0,
+    "OTHER": 0
+}
+
+
+def get_cat_copy():
+    return copy.deepcopy(CAT)
+
+
+def get_cat_num_copy():
+    return copy.deepcopy(CAT_NUMS)
+
+
 def read_file(filename='text.txt'):
     content = ""
     try:
